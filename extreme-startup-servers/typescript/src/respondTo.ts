50e4b9a67d9e1ca03e2520ed73ed970f474e56cc
Paul Gribelyuk <paulg@palantir.com>
2017-03-03 17:14:53 +1100

    stuff

--- extreme-startup-servers/typescript/src/respondTo.ts
+++ extreme-startup-servers/typescript/src/respondTo.ts
@@ -1,15 +1,22 @@
 import * as _ from 'lodash'
 
 let what_is_q = /what is ([0-9]+) plus ([0-9]+)/;
-// let which_largest = /^.* largest: (.*)/; 
+// let what_is_q = /what is ([0-9]+) plus ([0-9]+)/;
+let which_largest = /^.* largest: (.*)/; 
+
 export function respondTo(query: string): string {
     let m = what_is_q.exec(query)
     if(m) {
-        console.log(m);
-        console.log(m[1]);
-        console.log(m[2]);
-        return (parseInt(m[0]) + parseInt(m[1])).toString();
-    }    
+        return (parseInt(m[1]) + parseInt(m[2])).toString();
+    }
+
+    m = which_largest.exec(query);
+    if(m) {
+        let str = m[1];
+        let arr = str.split(", ");
+        console.log(arr);
+        return _.max(arr.map(parseInt)).toString();
+    }
     return `Unknown query '${query}'`;
 }
 
