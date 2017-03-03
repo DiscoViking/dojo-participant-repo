da19c175720f3a647bd710ff19a4dd5f14c731fd
Paul Gribelyuk <paulg@palantir.com>
2017-03-03 17:09:19 +1100

    asdf

--- extreme-startup-servers/typescript/src/respondTo.ts
+++ extreme-startup-servers/typescript/src/respondTo.ts
@@ -6,8 +6,8 @@ export function respondTo(query: string): string {
     let m = what_is_q.exec(query)
     if(m) {
         console.log(m);
-        console.log(m[0]);
         console.log(m[1]);
+        console.log(m[2]);
         return (parseInt(m[0]) + parseInt(m[1])).toString();
     }    
     return `Unknown query '${query}'`;
